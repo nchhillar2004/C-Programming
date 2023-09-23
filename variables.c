58f1502384f89df6b689c11cb2727fdda4eb68b9
nchhillar2004 <nchhillar2004@gmail.com>
2023-09-23 20:36:17 +0530

    1

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    float b = 8.5;
+    char c = 'u';
+    int d = 40 + a;
+    // %d for integers
+    printf("The value of a is %d \n", a);
+    // %f for real values/ float values
+    printf("The value of b is %f \n", b);
+    // %c for characters/ string
+    printf("The value of c is %c \n", c);
+    printf("The value of d is %d \n", d);
+    return 0;
+}
